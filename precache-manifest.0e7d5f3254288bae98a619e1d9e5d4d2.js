c8c36c0645b2a0b77199bf6161cb465509e851e8
Anant Saxena <95603053+anantsaxena5454@users.noreply.github.com>
2023-09-22 16:21:27 +0530

    Updates

---
+++ precache-manifest.0e7d5f3254288bae98a619e1d9e5d4d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c73e15250977d306276",
+    "url": "/TextUtils/static/css/main.b9541e9e.chunk.css"
+  },
+  {
+    "revision": "3c73e15250977d306276",
+    "url": "/TextUtils/static/js/main.3c73e152.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "7f6d7ce7a511d22ff0f37952e72df23e",
+    "url": "/TextUtils/index.html"
+  }
+];
